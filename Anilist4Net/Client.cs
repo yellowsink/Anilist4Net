64630b1198aa90871df5b073b46d53bbb0f3d281
cainy-a <cainy-a@users.noreply.github.com>
2020-12-24 15:35:55 +0000

    Media (mostly)

--- Anilist4Net/Client.cs
+++ Anilist4Net/Client.cs
@@ -7,9 +7,7 @@ namespace Anilist4Net
 {
 	public class Client
 	{
-		public async Task<User> GetUserByName(string username)
-		{
-			var query = @"query ($username: String) { User (name: $username) {
+		private static readonly string UserQueryReturn = @"{
 	id
 	name
 	aboutMd: about(asHtml: false)
@@ -49,7 +47,209 @@ namespace Anilist4Net
 	donatorBadge
 	moderatorStatus
 	updatedAt
-} }";
+}";
+
+		private static readonly string MediaQueryReturn = @"{
+    id
+    idMal
+    title {
+      romaji
+      english
+      native
+    }
+    type
+    format
+    status(version: 2)
+    descriptionMd: description(asHtml: false)
+    descriptionHtml: description(asHtml: true)
+    startDate {
+      year
+      month
+      day
+    }
+    endDate {
+      year
+      month
+      day
+    }
+    season
+    seasonYear
+    seasonInt
+    episodes
+    duration
+    chapters
+    volumes
+    countryOfOrigin
+    isLicensed
+    source(version: 2)
+    hashtag
+    trailer {
+      id
+      site
+      thumbnail
+    }
+    updatedAt
+    coverImage {
+      extraLarge
+      large
+      medium
+      color
+    }
+    bannerImage
+    genres
+    synonyms
+    averageScore
+    meanScore
+    popularity
+    isLocked
+    trending
+    favourites
+    tags {
+      id
+      name
+      description
+      category
+      rank
+      isGeneralSpoiler
+      isMediaSpoiler
+      isAdult
+    }
+    relations {
+      edges {
+        node {
+          id
+        }
+        relationType
+      }
+    }
+    characters {
+      edges {
+        node {
+          id
+        }
+        voiceActors {
+          id
+        }
+      }
+    }
+    staff {
+      edges {
+        node {
+          id
+        }
+        role
+      }
+    }
+    studios {
+      edges {
+        node {
+          id
+        }
+        isMain
+      }
+    }
+    isAdult
+    nextAiringEpisode {
+      id
+      airingAt
+      timeUntilAiring
+      episode
+      mediaId
+    }
+    airingSchedule {
+      nodes {
+        id
+        airingAt
+        timeUntilAiring
+        episode
+        mediaId
+      }
+    }
+    trends {
+      nodes {
+        mediaId
+        date
+        trending
+        averageScore
+        popularity
+        inProgress
+        releasing
+        episode
+      }
+    }
+    externalLinks {
+      id
+      url
+      site
+    }
+    streamingEpisodes {
+      title
+      thumbnail
+      url
+      site
+    }
+    rankings {
+      id
+      rank
+      type
+      format
+      year
+      season
+      allTime
+      context
+    }
+    reviews {
+      nodes {
+        id
+        userId
+        mediaId
+        mediaType
+        summary
+        bodyMd: body(asHtml: false)
+        bodyHtml: body(asHtml: true)
+        rating
+        ratingAmount
+        score
+        private
+        siteUrl
+        createdAt
+        updatedAt
+      }
+    }
+    recommendations {
+      nodes {
+        id
+        rating
+        media {
+          id
+        }
+        mediaRecommendation {
+          id
+        }
+        user {
+          id
+        }
+      }
+    }
+    stats {
+      scoreDistribution {
+        score
+        amount
+      }
+      statusDistribution {
+        status
+        amount
+      }
+    }
+    siteUrl
+    autoCreateForumThread
+    isRecommendationBlocked
+    modNotes
+  }";
+
+		public async Task<User> GetUserByName(string username)
+		{
+			var query         = $"query ($username: String) {{ User (name: $username) {UserQueryReturn} }}";
 			var request       = new GraphQLRequest {Query = query, Variables = new {username}};
 			var graphQlClient = new GraphQLHttpClient("https://graphql.anilist.co", new SystemTextJsonSerializer());
 
@@ -60,53 +260,31 @@ namespace Anilist4Net
 
 		public async Task<User> GetUserById(int id)
 		{
-			var query = @"query ($id: Int) { User (id: $id) {
-	id
-	name
-	aboutMd: about(asHtml: false)
-	aboutHtml:about(asHtml: true)
-	avatar {
-		large
-		medium
-	}
-	bannerImage
-	options {
-		titleLanguage
-		displayAdultContent
-		airingNotifications
-		profileColor
-	}
-	mediaListOptions {
-		scoreFormat
-		rowOrder
-		animeList {
-				sectionOrder
-				splitCompletedSectionByFormat
-				customLists
-				advancedScoring
-				advancedScoringEnabled
-			}
-		mangaList {
-			sectionOrder
-			splitCompletedSectionByFormat
-			customLists
-			advancedScoring
-			advancedScoringEnabled
-		}
-	}
-	unreadNotificationCount
-	siteUrl
-	donatorTier
-	donatorBadge
-	moderatorStatus
-	updatedAt
-} }";
+			var query         = $"query ($id: Int) {{ User (id: $id) {UserQueryReturn} }}";
 			var request       = new GraphQLRequest {Query = query, Variables = new {id}};
 			var graphQlClient = new GraphQLHttpClient("https://graphql.anilist.co", new SystemTextJsonSerializer());
 
 			var response = await graphQlClient.SendQueryAsync<UserResponse>(request);
 
 			return response.Data.User;
+		}
+
+		public async Task GetMediaById(int id)
+		{
+			var query         = $"query ($id: int) {{ Media (id: $id) {MediaQueryReturn} }}";
+			var request       = new GraphQLRequest {Query = query, Variables = new {id}};
+			var graphQlClient = new GraphQLHttpClient("https://graphql.anilist.co", new SystemTextJsonSerializer());
+
+			var response = await graphQlClient.SendQueryAsync<MediaResponse>(request);
+		}
+
+		public async Task GetMediaByMalId(int id)
+		{
+			var query         = $"query ($id: int) {{ Media (idMal: $id) {MediaQueryReturn} }}";
+			var request       = new GraphQLRequest {Query = query, Variables = new {id}};
+			var graphQlClient = new GraphQLHttpClient("https://graphql.anilist.co", new SystemTextJsonSerializer());
+
+			var response = await graphQlClient.SendQueryAsync<MediaResponse>(request);
 		}
 	}
 }
---
+++ Anilist4Net/Media.cs
@@ -0,0 +1,334 @@
+﻿namespace Anilist4Net
+{
+	public class Media
+	{
+		private MediaTitle          _mediaTitle     { get; set; }
+		public  int                 Id              { get; set; }
+		public  int?                IdMal           { get; set; }
+		public  MediaTypes          Type            { get; set; }
+		public  MediaFormats        Format          { get; set; }
+		public  MediaStatuses       Status          { get; set; }
+		public  string              DescriptionMd   { get; set; }
+		public  string              DescriptionHtml { get; set; }
+		private FuzzyDate           _startDate      { get; set; }
+		private FuzzyDate           _endDate        { get; set; }
+		public  Seasons             Season          { get; set; }
+		public  int?                SeasonYear      { get; set; }
+		public  int?                SeasonInt       { get; set; }
+		public  int?                Episodes        { get; set; }
+		public  int?                Duration        { get; set; }
+		public  int?                Chapters        { get; set; }
+		public  int?                Volumes         { get; set; }
+		public  string              CountryOfOrigin { get; set; }
+		public  bool                IsLicensed      { get; set; }
+		public  MediaSources        Source          { get; set; }
+		public  string              Hashtag         { get; set; }
+		public  MediaTrailer        Trailer         { get; set; }
+		public  int                 UpdatedAt       { get; set; }
+		private MediaCoverImage     _coverImage     { get; set; }
+		public  string              BannerImage     { get; set; }
+		public  string[]            Genres          { get; set; }
+		public  string[]            Synonyms        { get; set; }
+		public  int                 AverageScore    { get; set; }
+		public  int                 MeanScore       { get; set; }
+		public  int                 Popularity      { get; set; }
+		public  bool                IsLocked        { get; set; }
+		public  int                 Trending        { get; set; }
+		public  int                 Favourites      { get; set; }
+		public  MediaTag[]          Tags            { get; set; }
+		private MediaConnection     _relations      { get; set; }
+		private CharacterConnection _characters     { get; set; }
+		private StaffConnection     _staff          { get; set; }
+		private StudioConnection    _studios        { get; set; }
+
+		public bool IsAdult { get; set; }
+
+		// public AiringSchedule NextAiringEpisode{ get; set; }
+		private AiringScheduleConnection _airingSchedule       { get; set; }
+		private MediaTrendConnection     _trends               { get; set; }
+		public  MediaExternalLink[]      ExternalLinks         { get; set; }
+		public  MediaStreamingEpisode[]  StreamingEpisodes     { get; set; }
+		public  MediaRanking[]           Rankings              { get; set; }
+		private ReviewConnection         _reviews              { get; set; }
+		private ReccomendationConnection _reccomendations      { get; set; }
+		public  MediaStats               Stats                 { get; set; }
+		public  string                   SiteUrl               { get; set; }
+		public  bool                     AutoCreateForumThread { get; set; }
+		public  string                   ModNotes              { get; set; }
+	}
+
+	internal class MediaResponse
+	{
+		public Media Media { get; set; }
+	}
+
+	internal class MediaTitle
+	{
+		public string Romaji  { get; set; }
+		public string English { get; set; }
+		public string Native  { get; set; }
+	}
+
+	internal class FuzzyDate
+	{
+		public int Year  { get; set; }
+		public int Month { get; set; }
+		public int Day   { get; set; }
+	}
+
+	public class MediaTrailer
+	{
+		public int    Id        { get; set; }
+		public string Site      { get; set; }
+		public string Thumbnail { get; set; }
+	}
+
+	internal class MediaCoverImage
+	{
+		public string ExtraLarge { get; set; }
+		public string Large      { get; set; }
+		public string Medium     { get; set; }
+		public string Color      { get; set; }
+	}
+
+	public class MediaTag
+	{
+		public int    Id               { get; set; }
+		public string Name             { get; set; }
+		public string Description      { get; set; }
+		public string Category         { get; set; }
+		public int    Rank             { get; set; }
+		public bool   IsGeneralSpoiler { get; set; }
+		public bool   IsMediaSpoiler   { get; set; }
+		public bool   IsAdult          { get; set; }
+	}
+
+	internal class MediaConnection
+	{
+		public MediaEdge[] Edges { get; set; }
+	}
+
+	internal class MediaEdge
+	{
+		public MediaNodePlaceholder Node         { get; set; }
+		public MediaRelationType    RelationType { get; set; }
+	}
+
+	internal class MediaNodePlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class CharacterConnection
+	{
+		public CharacterEdge[] Edges { get; set; }
+	}
+
+	internal class CharacterEdge
+	{
+		public CharacterNodePlaceholder Node        { get; set; }
+		public VoiceActorPlaceholder[]  VoiceActors { get; set; }
+	}
+
+	internal class CharacterNodePlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class VoiceActorPlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class StaffConnection
+	{
+		public StaffEdge[] Edges { get; set; }
+	}
+
+	internal class StaffEdge
+	{
+		public StaffNodePlaceholder Node { get; set; }
+		public string               Role { get; set; }
+	}
+
+	internal class StaffNodePlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class StudioConnection
+	{
+		public StudioEdge[] Edges { get; set; }
+	}
+
+	internal class StudioEdge
+	{
+		public StudioNodePlaceholder Node   { get; set; }
+		public bool                  IsMain { get; set; }
+	}
+
+	internal class StudioNodePlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class AiringScheduleConnection
+	{
+		public AiringScheduleNodePlaceholder[] Nodes { get; set; }
+	}
+
+	internal class AiringScheduleNodePlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class MediaTrendConnection
+	{
+		// public MediaTrend[] Nodes { get; set; }
+	}
+
+	public class MediaExternalLink
+	{
+		public int    Id   { get; set; }
+		public string Url  { get; set; }
+		public string Site { get; set; }
+	}
+
+	public class MediaStreamingEpisode
+	{
+		public string Title     { get; set; }
+		public string Thumbnail { get; set; }
+		public string Url       { get; set; }
+		public string Site      { get; set; }
+	}
+
+	public class MediaRanking
+	{
+		public int          Id      { get; set; }
+		public int          Rank    { get; set; }
+		public string       Type    { get; set; }
+		public MediaFormats Format  { get; set; }
+		public int          Year    { get; set; }
+		public Seasons      Season  { get; set; }
+		public bool         AllTime { get; set; }
+		public string       Context { get; set; }
+	}
+
+	internal class ReviewConnection
+	{
+		// public Review[] Nodes { get; set; }
+	}
+
+	internal class ReccomendationConnection
+	{
+		public ReccomendationNode[] Nodes { get; set; }
+	}
+
+	internal class ReccomendationNode
+	{
+		public int                            Id                  { get; set; }
+		public int                            Rating              { get; set; }
+		public MediaNodePlaceholder           Media               { get; set; }
+		public MediaReccomendationPlaceholder MediaReccomendation { get; set; }
+		public UserPlaceholder                User                { get; set; }
+	}
+
+	internal class MediaReccomendationPlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	internal class UserPlaceholder
+	{
+		public int Id { get; set; }
+	}
+
+	public class MediaStats
+	{
+		public ScoreDistribution[]  ScoreDistribution  { get; set; }
+		public StatusDistribution[] StatusDistribution { get; set; }
+	}
+
+	public class ScoreDistribution
+	{
+		public int Score  { get; set; }
+		public int Amount { get; set; }
+	}
+
+	public class StatusDistribution
+	{
+		public MediaStatuses Status { get; set; }
+		public int           Amount { get; set; }
+	}
+
+	// ReSharper disable InconsistentNaming
+	// ReSharper disable IdentifierTypo
+	public enum MediaTypes
+	{
+		ANIME,
+		MANGA
+	}
+
+	public enum MediaFormats
+	{
+		TV,
+		TV_SHORT,
+		MOVIE,
+		SPECIAL,
+		OVA,
+		ONA,
+		MUSIC,
+		MANGA,
+		NOVEL,
+		ONE_SHOT
+	}
+
+	public enum MediaStatuses
+	{
+		FINISHED,
+		RELEASING,
+		NOT_YET_RELEASED,
+		CANCELLED,
+		HIATUS
+	}
+
+	public enum Seasons
+	{
+		WINTER,
+		SPRING,
+		SUMMER,
+		AUGUST
+	}
+
+	public enum MediaSources
+	{
+		ORIGINAL,
+		MANGA,
+		LIGHT_NOVEL,
+		VISUAL_NOVEL,
+		VIDEO_GAME,
+		OTHER,
+		NOVEL,
+		DOUJINSHI,
+		ANIME
+	}
+
+	public enum MediaRelationType
+	{
+		ADAPTATION,
+		PREQUEL,
+		SEQUEL,
+		PARENT,
+		SIDE_STORY,
+		CHARACTER,
+		SUMMARY,
+		ALTERNATIVE,
+		SPIN_OFF,
+		OTHER,
+		SOURCE,
+		COMPILATION,
+		CONTAINS
+	}
+	// ReSharper restore IdentifierTypo
+	// ReSharper restore InconsistentNaming
+}
