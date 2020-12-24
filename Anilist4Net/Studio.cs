0d676e576cf28bee585605af240f9e3bb039e4f9
cainy-a <cainy-a@users.noreply.github.com>
2020-12-24 20:52:53 +0000

    Studio XML-DOC

--- Anilist4Net/Studio.cs
+++ Anilist4Net/Studio.cs
@@ -4,13 +4,37 @@ namespace Anilist4Net
 {
 	public class Studio
 	{
-		public  int                 Id                { get; set; }
-		public  string              Name              { get; set; }
-		public  bool                IsAnimationStudio { get; set; }
-		private MediaNodeConnection Media             { get; set; }
-		public  int[]               MediaIds          => Media.Nodes.Select(n => n.Id).ToArray();
-		public  string              SiteUrl           { get; set; }
-		public  int                 Favourites        { get; set; }
+		/// <summary>
+		///     The studio ID
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		///     The studio name
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		///     If the studio does animation, or some other kind of company
+		/// </summary>
+		public bool IsAnimationStudio { get; set; }
+
+		private MediaNodeConnection Media { get; set; }
+
+		/// <summary>
+		///     The IDs of media the studio has worked on
+		/// </summary>
+		public int[] MediaIds => Media.Nodes.Select(n => n.Id).ToArray();
+
+		/// <summary>
+		///     The studio's Anilist URL
+		/// </summary>
+		public string SiteUrl { get; set; }
+
+		/// <summary>
+		///     How many users favourited the studio
+		/// </summary>
+		public int Favourites { get; set; }
 	}
 
 	internal class StudioResponse
