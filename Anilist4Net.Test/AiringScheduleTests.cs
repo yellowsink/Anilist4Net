7d91a5ffe19d1518102195d30b9a3c678b915c43
cainy-a <cainy-a@users.noreply.github.com>
2020-12-25 14:45:05 +0000

    Airing Schedule Tests

---
+++ Anilist4Net.Test/AiringScheduleTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Anilist4Net.Test
+{
+	[TestFixture]
+	public class AiringScheduleTests
+	{
+		[Test]
+		public async Task
+			HypnosisMicEp1ById() // First episode of HYPNOSISMIC because it was all I could think of that was airing.
+		{
+			var client         = new Client();
+			var airingSchedule = await client.GetAiringScheduleById(293185);
+
+			Assert.AreEqual(293185,     airingSchedule.Id);
+			Assert.AreEqual(1601650800, airingSchedule.AiringAt);
+			Assert.AreEqual(1,          airingSchedule.Episode);
+			Assert.AreEqual(113652,     airingSchedule.MediaId);
+		}
+	}
+}
