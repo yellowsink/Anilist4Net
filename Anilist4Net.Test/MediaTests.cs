492ee9568b1e721a28aaa95de2391b30f04e2e57
DeadlyEmbrace <ralphviktor@ninetaillabs.com>
2021-06-10 13:41:18 +0200

    Update Anilist4Net.Test/MediaTests.cs
    Co-authored-by: Cain Atkinson <19270622+yellowsink@users.noreply.github.com>

--- Anilist4Net.Test/MediaTests.cs
+++ Anilist4Net.Test/MediaTests.cs
@@ -99,7 +99,7 @@ namespace Anilist4Net.Test
 		// Caters for cases where there are more than one Mal Id that is the same (for example an anime and a manga) by allowing the user to specify the media type they expect
         [TestCase(MediaTypes.ANIME, "Yakusoku no Neverland 2")]
         [TestCase(MediaTypes.MANGA, "Vermillion")]
-		public async Task SettingExpectedMediaTypeCorrectlyDifferentiateMatchingMalIds(MediaTypes typeToRetrieve, string expectedTitle)
+		public async Task MalIdByTypeTest(MediaTypes typeToRetrieve, string expectedTitle)
         {
             var media = await client.GetMediaByMalId(39617, typeToRetrieve);
             AreEqual(typeToRetrieve, media.Type);
